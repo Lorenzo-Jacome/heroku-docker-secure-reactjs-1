b51c4b087fd6da46f44688848314eda596ae4eb4
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2022-04-04 21:02:03 -0500

    Add drawer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import ReactDOM from 'react-dom';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './drawer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hola Lorenzo.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          
-        </a>
-      </header>
+      <Demo />
     </div>
   );
 }

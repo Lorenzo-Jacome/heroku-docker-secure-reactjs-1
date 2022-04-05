86c84d5f66d8a61876c3db53e59a308acc1b634d
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2022-04-04 20:29:15 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Hola Ruben.
+          Edit <code>src/App.js</code> Hola Lorenzo.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Lorenzo Jácome
+          
         </a>
       </header>
     </div>

3698161c14f8bb1680af873bd7282175369743ae
Lorenzo Jácome <lorenzo.jacome@hotmail.com>
2022-04-04 20:28:13 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Lorenzo Jácome
         </a>
       </header>
     </div>
